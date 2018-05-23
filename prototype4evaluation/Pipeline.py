321f5cccbc51907e96b26cb7bf3d9eed4bf5452a
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-05-23 14:50:06 -0400

    Add basic pipeline

--- prototype4evaluation/Pipeline.py
+++ prototype4evaluation/Pipeline.py
@@ -1,8 +1,34 @@
 from prototype4evaluation.pipeline.common import BasePipeline
 
 class Pipeline(BasePipeline):
-    def __init__(self):
+    def __init__(self,
+                 algorithm_constructor,
+                 algorithm_arguments,
+                 evaluation_constructor,
+                 evaluation_arguments,
+                 repeats=5):
         super().__init__()
+        self.algorithm_constructor = evaluation_constructor
+        self.algorithm_arguments = evaluation_arguments
+        self.evaluation_constructor = evaluation_constructor
+        self.evaluation_arguments = evaluation_arguments
 
-    def run(self):
-        pass
+    def _run_parallel(self):
+        raise NotImplementedError('Future work.')
+
+    def start(self):
+        """
+        Creates an instance of the algorithm
+        Returns:
+            An Algorithm and an Evaluation Scheme.
+        """
+        # abstracted out so that we can eventually do this in parallel.
+        alg = self.algorithm_constructor(**self.algorithm_arguments)
+        eval = self.evaluation_constructor(**self.evaluation_arguments)
+        return alg, eval
+
+    def run(self, training_epochs):
+        algorithm, evaluation = self.start()
+
+        for i in range(training_epochs):
+            algorithm.train_step()
