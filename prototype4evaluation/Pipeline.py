09afe095d5cf604f7b525971602d077ee95e5f3e
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-05-23 14:16:52 -0400

    Add skeleton

---
+++ prototype4evaluation/Pipeline.py
@@ -0,0 +1,8 @@
+from prototype4evaluation.pipeline.common import BasePipeline
+
+class Pipeline(BasePipeline):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
